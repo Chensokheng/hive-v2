7176a7d30db41e4df629c5f7eecbf1e58ce36f3e
sokheng <chensokheng6@gmail.com>
2025-08-13 13:12:37 +0700

    chore: static

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { use } from "react";
 import { useTranslations } from "next-intl";
+import { setRequestLocale } from "next-intl/server";
 
-export default function AboutPage() {
+export default function AboutPage({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = use(params);
+  setRequestLocale(locale);
   const t = useTranslations("AboutPage");
 
   return <div>{t("title")}</div>;
