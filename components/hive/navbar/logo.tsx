9c0c03c530b4f58e532f61a733ddfbf14a87ec33
sokheng <chensokheng6@gmail.com>
2025-08-15 14:07:57 +0700

    chore: navbar

--- components/hive/navbar/logo.tsx
+++ components/hive/navbar/logo.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Image from "next/image";
+import { Link } from "@/i18n/navigation";
 
 export default function Logo() {
   return (
-    <div className="relative w-[60px] h-[30px] sm:w-[80px] sm:h-[40px] md:w-[100px] md:h-[50px]">
-      <Image
-        src={"/assets/logo.png"}
-        alt="logo"
-        fill
-        sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, 100px"
-        className="object-contain"
-      />
-    </div>
+    <Link href={"/"}>
+      <div className="relative w-[60px] h-[30px] sm:w-[80px] sm:h-[40px] md:w-[100px] md:h-[50px]">
+        <Image
+          src={"/assets/logo.png"}
+          alt="logo"
+          fill
+          sizes="(max-width: 640px) 60px, (max-width: 768px) 80px, 100px"
+          className="object-contain"
+        />
+      </div>
+    </Link>
   );
 }
