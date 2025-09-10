ddde8c81332adc969dc2578c876dffb92afbc8fc
sokheng <chensokheng6@gmail.com>
2025-09-10 09:35:57 +0700

    chore: cache image for 30 day

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,21 @@ import createNextIntlPlugin from "next-intl/plugin";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    minimumCacheTTL: 2678400, // 31 days
     remotePatterns: [
       {
         protocol: "https",
         hostname: "unpkg.com",
+        port: "",
+        pathname: "/**",
+        search: "",
       },
       {
         protocol: "https",
         hostname: "d3gen0htoikr36.cloudfront.net",
+        port: "",
+        pathname: "/**",
+        search: "",
       },
     ],
   },
