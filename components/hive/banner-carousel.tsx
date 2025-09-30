0eee252cbf560dabbce1ff523207baf9ea098b84
sokheng <chensokheng6@gmail.com>
2025-09-30 11:58:03 +0700

    feat: banner

--- components/hive/banner-carousel.tsx
+++ components/hive/banner-carousel.tsx
@@ -2,10 +2,12 @@
 
 import React from "react";
 import { rowdies } from "@/fonts";
+import { useRouter } from "@/i18n/navigation";
 import { AsyncImage } from "loadable-image";
 import { Blur } from "transitions-kit";
 
-import { cn } from "@/lib/utils";
+import { cn, getImageUrl } from "@/lib/utils";
+import useGetBanner from "@/hooks/use-get-banner";
 
 interface BannerItem {
   id: string;
@@ -40,49 +42,75 @@ const defaultBanners: BannerItem[] = [
 export default function BannerCarousel({
   banners = defaultBanners,
 }: BannerCarouselProps) {
+  const { data, isLoading } = useGetBanner();
+  const router = useRouter();
+  if (isLoading) {
+    return <h1>Loading..</h1>;
+  }
+  const handleCta = (
+    url: string | null,
+    merchantLength: number,
+    id: number
+  ) => {
+    if (url) {
+      router.push(url);
+    } else if (merchantLength) {
+      router.push("/banner/" + id);
+    }
+  };
+
   return (
     <div className="flex overflow-x-auto gap-4 pr-16 scroll-smooth snap-x snap-mandatory hide-scroll ml-5 ">
-      {banners.map((banner) => (
-        <div key={banner.id}>
-          <div className="h-44 w-[calc(100vw-3rem)] sm:w-[calc(448px-3rem)] flex-shrink-0 relative snap-start">
-            <AsyncImage
-              src={banner.image}
-              Transition={Blur}
-              style={{ width: "100%", height: 176, borderRadius: 16 }}
-              loader={<div className="bg-gray-300" />}
-            />
-            <div className="absolute bottom-5 left-5 text-white">
-              <div>
-                <h1
-                  className={cn(
-                    "text-[22px] leading-[28px] font-bold traking-[-0.4px]",
-                    rowdies.className
-                  )}
-                >
-                  {banner.title}
-                </h1>
-                <p className="text-sm leading-[18px]">{banner.description}</p>
-              </div>
+      {data?.data.map((banner) => {
+        const titleColor = `text-[${banner.titleColor}]`;
+        const descriptionColor = `text-[${banner.subtitleColor}]`;
+        const ctaButtonTitleColor = `text-[${banner.ctaButtonTitleColor}]`;
 
-              <button
-                className="
-                bg-primary/10 backdrop-blur-md 
-                shadow-lg shadow-black/20
-                text-white font-semibold text-sm
-                transition-all duration-300
-                py-2 px-4 inline-block mt-3
-                border-t border-b border-white/30
-                border-l-0 border-r-0 rounded-full
-                hover:bg-primary/20
-              "
-                onClick={banner.onButtonClick}
-              >
-                {banner.buttonText}
-              </button>
+        return (
+          <div key={banner.id}>
+            <div className="h-44 w-[calc(100vw-3rem)] sm:w-[calc(448px-3rem)] flex-shrink-0 relative snap-start">
+              <AsyncImage
+                src={getImageUrl(banner.image)}
+                Transition={Blur}
+                style={{ width: "100%", height: 176, borderRadius: 16 }}
+                loader={<div className="bg-gray-300" />}
+              />
+              <div className="absolute bottom-5 left-5 text-white">
+                <div>
+                  <h1
+                    className={cn(
+                      "text-[22px] leading-[28px] font-bold traking-[-0.4px]",
+                      rowdies.className,
+                      titleColor
+                    )}
+                  >
+                    {banner.title}
+                  </h1>
+                  <p className={cn("text-sm leading-[18px]", descriptionColor)}>
+                    {banner.subtitle}
+                  </p>
+                </div>
+
+                <button
+                  className={cn(
+                    "bg-primary/10 backdrop-blur-md  shadow-lg shadow-black/20 font-semibold text-sm transition-all duration-300 py-2 px-4 inline-block mt-3 border-t border-b border-white/30 border-l-0 border-r-0 rounded-full hover:bg-primary/20",
+                    ctaButtonTitleColor
+                  )}
+                  onClick={() =>
+                    handleCta(
+                      banner.ctaButtonUrl,
+                      banner.merchants.length,
+                      banner.id
+                    )
+                  }
+                >
+                  {banner.ctaButtonTitle}
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
