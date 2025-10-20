e3e450c65afc90c1cd19f5cfa34098f4d29e5640
sokheng <chensokheng6@gmail.com>
2025-10-20 20:30:03 +0700

    chore: mini app type

--- components/mini/js-bridge-listener.tsx
+++ components/mini/js-bridge-listener.tsx
@@ -12,8 +12,6 @@ import { JSBridge } from "@/lib/js-bridge";
 export default function JsBridgeListener() {
   const initFetchUser = async () => {
     const { token, client_id } = await generateMmsToken();
-    toast.success(token.access_token);
-    toast.success(client_id);
     JSBridge.call(
       "getUserInfo",
       JSON.stringify({
